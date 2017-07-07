bee97af48acca72ab64f95c7b914f9e8e79802f1
Fangshanshan <643915247@qq.com>
2017-07-07 09:42:12 +0800

    dsafdsafdsfds

---
+++ frame-common/src/main/java/com/frame/properties/untils/PropertiesUntils.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2015-2017 China CO-OP ELECTRONIC COMMERCE CO. LTD. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * China CO-OP ELECTRONIC COMMERCE CO. LTD. ("Confidential Information").
+ * It may not be copied or reproduced in any manner without the express
+ * written permission of China CO-OP ELECTRONIC COMMERCE CO. LTD.
+ */
+
+package com.frame.properties.untils;
+
+public class PropertiesUntils {
+
+}
+
